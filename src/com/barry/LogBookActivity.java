54771596bff42b317471a3e1ef9452063327eb00
Barry Tarlton <btarlton@gmail.com>
2012-04-13 14:16:38 -0400

    Got JSON working to convert Mark to JSON

--- src/com/barry/LogBookActivity.java
+++ src/com/barry/LogBookActivity.java
@@ -1,6 +1,8 @@
 package com.barry;
 
 import com.barry.domain.Mark;
+import com.barry.domain.service.IMarkService;
+import com.barry.domain.service.MarkService;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,12 +14,15 @@ import android.widget.EditText;
 public class LogBookActivity extends Activity {
 	private EditText markDetailsEditText;
 	private Button markButton;
+	private IMarkService markService;
 	
 	private Mark mark;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        markService = new MarkService();
         setContentView(R.layout.main);
         
         markDetailsEditText = (EditText)findViewById(R.id.markDescription);
@@ -31,6 +36,7 @@ public class LogBookActivity extends Activity {
 				Log.d(LogBookActivity.class.getName(),"text:" + markDetailsEditText.getText().toString());
 				mark = new Mark();
 				mark.description = description;
+				markService.storeMark(mark);
 				
 				
 			}
--- src/com/barry/domain/service/MarkService.java
+++ src/com/barry/domain/service/MarkService.java
@@ -1,17 +1,40 @@
 package com.barry.domain.service;
 
+import android.util.Log;
+
 import com.barry.domain.Mark;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MarkService implements IMarkService{
 
+	private String tag = MarkService.class.getName();
+	
 	@Override
 	public void storeMark(Mark mark) {
 		// must store a Mark to the system(file or DB)
+		// get the current location
+		//TODO: get location
+		// let's turn it to JSON
+		String json = getJSONFromMark(mark);
+		Log.d(tag, "JSON=" + json);
 		
 		
 	}
 	
 	
+	public String getJSONFromMark(Mark mark){
+		String json = null;
+		Gson gson = new Gson();
+		try{	
+			json= gson.toJson(mark);
+		}catch (Exception e) {
+			Log.e(tag, "Caught exception trying to convert Mark to JSON. ERROR:"+e.toString());
+			e.printStackTrace();
+		}
+		
+		return json;
+	}
 	
 
 }
